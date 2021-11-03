80e25ca96ec9fda84bbe306be4ff87a25da1d5c5
Daniel Garnier-Moiroux <git@garnier.wf>
2021-11-03 21:22:52 +0100

    Better palindrome error messages

--- test_palindrome.py
+++ test_palindrome.py
@@ -7,36 +7,44 @@ from inspect import cleandoc
 class TestPalindrome(unittest.TestCase):
     def test_simple(self):
         from palindrome import palindrome
+        self.assertIsInstance(palindrome(
+            "kayak"), bool, "la fonction palindrome(...) doit renvoyer un bool, par exemple \"return True\"")
         self.assertTrue(palindrome("kayak"))
         self.assertTrue(palindrome("if fi"))
         self.assertFalse(palindrome("tracteur"))
 
     def test_punctuation(self):
         from palindrome import palindrome
+        self.assertIsInstance(palindrome(
+            "kayak"), bool, "la fonction palindrome(...) doit renvoyer un bool, par exemple \"return True\"")
         self.assertTrue(palindrome("Eva, Can I Stab Bats In A Cave?"))
 
     def test_display_ok(self):
         from palindrome import display_palindrome
         stdout = StringIO()
         with patch("sys.stdout", new=stdout):
+            display_palindrome("elle")
+            output = stdout.getvalue().strip("\n")
+            self.assertGreater(len(
+                output), 0, "la fonction display_palindrome(...) doit afficher des resultats, par exemple \"print(chaine)\"")
             expected = cleandoc("""
             elle
             elle
             OK""")
-            display_palindrome("elle")
-            output = stdout.getvalue().strip("\n")
             self.assertEqual(output, expected)
 
     def test_display_difference(self):
         from palindrome import display_palindrome
         stdout = StringIO()
         with patch("sys.stdout", new=stdout):
+            display_palindrome("cuisine")
+            output = stdout.getvalue().strip("\n")
+            self.assertGreater(len(
+                output), 0, "la fonction display_palindrome(...) doit afficher des resultats, par exemple \"print(chaine)\"")
             expected = cleandoc("""
             cuisine
             enisiuc
             ^^   ^^""")
-            display_palindrome("cuisine")
-            output = stdout.getvalue().strip("\n")
             self.assertEqual(output, expected)
 
 
